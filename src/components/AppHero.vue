64894ce3414ecf07377b0df13f937aa89076375d
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-26 16:18:11 +0200

    [FIX] fixed folder issue

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <img src="../assets/codecode.webp" alt="imgg">
+    </div>
+</template>
+
+<style>
+    img{
+        width: 400px;
+    }
+</style>
